683e1b81a05d836666af8b8f0cee047587ec5459
Morgan Senkal <morgan.senkal@metaltoad.com>
2016-09-12 17:18:08 -0700

    Updated books knowledge base with moar data

--- prolog/books.pl
+++ prolog/books.pl
@@ -1,7 +1,7 @@
-book('the hobbit', tolkein).
-book('the fellowship of the ring', tolkein).
-book('the two towers', tolkein).
-book('the return of the king', tolkein).
+book('the hobbit', tolkien).
+book('the fellowship of the ring', tolkien).
+book('the two towers', tolkien).
+book('the return of the king', tolkien).
 book('fingersmith', waters).
 book('the paying guests', waters).
 book('tipping the velvet', waters).
@@ -16,10 +16,25 @@ book('clash of kings', martin).
 book('storm of swords', martin).
 book('feast for crows', martin).
 book('dance with dragons', martin).
+book('the little stranger', waters).
+book('the night watch', waters).
+book('name of the wind', rothfuss).
+book("wise man's fear", rothfuss).
+book('doors of stone', rothfuss).
+book('carrie', king).
+book('cujo', king).
+book('christine', king).
+book('it', king).
+book("salem's lot", king).
 
-author(tolkein, fantasy).
+
+
+
+author(tolkien, fantasy).
 author(sanderson, fantasy).
 author(waters, 'historical fiction').
 author(martin, fantasy).
+author(rothfuss, fantasy).
+author(king, horror).
 
 book_info(X,Y,Z) :- book(X, Y), author(Y,Z).
